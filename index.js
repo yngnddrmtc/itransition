621c2a22ad409cd92fa903216608737c67283c97
Aziz <aziznazirov656@gmail.com>
2023-05-25 22:04:39 +0500

    fixed error messages

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const moves = process.argv.slice(2);
 const main = require("./main");
 
 if (2 > moves.length) {
-  console.log("you should pass atleast 3 moves");
+  console.log("You cannot pass less than 3 options, instead, try this: 1 2 3");
   return;
 }
 
 if (moves.length % 2 === 0) {
-  console.log("you should pass odd number of moves");
+  console.log("You should pass odd number (e.g.: 1 2 3 4 5)");
   return;
 }
 
-if (new Set(moves).size !== moves.length) {
-  console.log("you should pass different moves");
+if ([...new Set(moves)].length !== moves.length) {
+  console.log("You cannot pass the same options several times");
   return;
 }
 
